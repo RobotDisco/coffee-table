14dfec0265157f391ec09ef0446e49f89e3961a2
Gaelan D'costa <gaelan@tulip.io>
2018-02-10 22:13:01 -0500

    First real DB component functionality, + integration test

--- src/clj/coffee_table/component/database.clj
+++ src/clj/coffee_table/component/database.clj
@@ -1,12 +1,56 @@
 (ns coffee-table.component.database
-  (:require [com.stuartsierra.component :as component]
-            [clojure.java.jdbc :as jdbc]))
+  (:require [coffee-table.db.visits :as dbv]
+            [coffee-table.model :as m]
+            [com.stuartsierra.component :as component]
+            [clojure.java.jdbc :as jdbc]
+            [hugsql.core :as hugsql]
+            [migratus.core :as migrations]
+            [taoensso.timbre :as timbre]
+            [schema.core :as s])
+  (:import [java.sql PreparedStatement]))
 
-(defrecord Database []
+(timbre/refer-timbre)
+
+(defn to-date [sql-date]
+  (-> sql-date (.getTime) (java.util.Date.)))
+
+(extend-protocol jdbc/IResultSetReadColumn
+  java.sql.Date
+  (result-set-read-column [value metadata index]
+    (to-date value)))
+
+(extend-type java.util.Date
+  jdbc/ISQLParameter
+  (set-parameter [value ^PreparedStatement stmt idx]
+    (.setTimestamp stmt idx (java.sql.Timestamp. (.getTime value)))))
+
+(s/defrecord Database [spec migratus connection]
   component/Lifecycle
-  (start [this])
-  (stop [this]))
+  (start [this]
+    (info ::starting)
+    (let [conn (or connection (jdbc/get-connection spec))]
+      (assoc this :connection conn)))
+  (stop [this]
+    (info ::stopping)
+    (when-let [conn (:connection this)]
+      (.close conn))
+    (assoc this :connection nil)))
 
-(defn new-database
- []
- (map->Database {}))
+(s/defn new-database :- Database
+  [m]
+  (map->Database m))
+
+(s/defn insert-visit! :- s/Int
+  "Insert a visit record into the DB. return the newly created record's ID"
+  [component :- Database
+   visit :- m/Visit]
+  (let [{:keys [id]} (dbv/insert-visit! (:spec component) visit)]
+    id))
+
+(s/defn get-visit :- m/Visit
+  "Fetch a visit from the DB with the provided ID"
+  [component :- Database
+   id :- s/Int]
+  (dbv/get-visit (:spec component) id))
+
+
--- test/clj/coffee_table/component/database_test.clj
+++ test/clj/coffee_table/component/database_test.clj
@@ -1,19 +1,45 @@
 (ns coffee-table.component.database-test
-  (:require  [clojure.test :as t :refer [deftest is]]
+  (:require [clojure.java.jdbc :as sql]
+   [clojure.test :as t :refer [deftest is]]
              [coffee-table.component.database :as sut]
+             [coffee-table.config :as ctcfg]
              [coffee-table.test.system :as cts]
              [com.stuartsierra.component :as component]
              [schema.core :as s]
              [schema.test]))
 
+(def config (ctcfg/config :test))
+
 (defn test-system
   "Create minimal system to test Visit logic functionality"
   []
   (component/system-using
-   (component/system-map)
+   (component/system-map
+    :db (sut/new-database {:spec (ctcfg/database-spec config)
+                           :migratus (ctcfg/migratus config)}))
    {}))
 
-(t/use-fixtures :once schema.test/validate-schemas (cts/with-system-fixture test-system))
+(t/use-fixtures :once schema.test/validate-schemas (cts/with-system-fixture test-system) cts/with-transaction-fixture)
 
 (deftest test-create-visit
-  (is (= 1 1)))
+  (let [db (:db cts/*system*)
+        visit-params {:cafe_name "Test Café"
+                      :visit_date (java.util.Date.)
+                      :beverage_ordered "Espresso"
+                      :beverage_rating 3}
+        visit-id (sut/insert-visit! db visit-params)]
+    (is (= {:id (:id visit-params)
+            :cafe_name (:cafe_name visit-params)
+            :visit_date (:visit_date visit-params)
+            :beverage_ordered (:beverage_ordered visit-params)
+            :beverage_rating (:beverage_rating visit-params)
+            :machine nil
+            :grinder nil
+            :roast nil
+            :beverage_notes nil
+            :service_rating nil
+            :service_notes nil
+            :ambience_rating nil
+            :ambience_notes nil
+            :other_notes nil}
+           (sut/get-visit db {:id 1})))))
