f9b44c51583c84184531cfcfe6ee19d023390c32
Gaelan D'costa <gaelan@tulip.io>
2018-02-11 21:53:57 -0500

    perform schema validation in dev repl

--- dev/user.clj
+++ dev/user.clj
@@ -1,7 +1,10 @@
 (ns user
   (:require [coffee-table.component.database :as db]
             [coffee-table.system :refer [new-system]]
-            [reloaded.repl :refer [system init start stop go reset reset-all]]))
+            [reloaded.repl :refer [system init start stop go reset reset-all]]
+            [schema.core :as s]))
+
+(s/set-fn-validation! true)
 
 (reloaded.repl/set-init! #(new-system :dev))
 
