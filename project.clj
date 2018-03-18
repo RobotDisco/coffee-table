14293f28cd0df31166cf193becc9b4270a934c6b
Gaelan D'costa <gdcosta@gmail.com>
2018-03-18 01:33:32 -0400

    Version 0.0.5-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject coffee-table "0.0.4"
+(defproject coffee-table "0.0.5-SNAPSHOT"
   :description "A site for Gaelan's café reviews"
   :url "http://github.com/RobotDisco/coffee-table"
   :license {:name "Eclipse Public License - v 1.0"
