05cd810ceb8091a69b8b527d70e79dc82bf22392
Gaelan D'costa <gdcosta@gmail.com>
2018-04-25 23:14:39 -0400

    Helper function for adding users into system
    Not super refined. But it works.

--- dev/user.clj
+++ dev/user.clj
@@ -1,15 +1,27 @@
 (ns user
   (:require [coffee-table.component.database :as db]
+            [coffee-table.component.users :as uc]
             [coffee-table.system :refer [new-system]]
             [reloaded.repl :refer [system init start stop go reset reset-all]]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [buddy.hashers :as bhash]))
 
 (s/set-fn-validation! true)
 
 (reloaded.repl/set-init! #(new-system :dev))
 
-(defn migrate []
+(defn migrate! []
   (db/migrate (:db system)))
 
-(defn rollback []
+(defn rollback! []
   (db/rollback (:db system)))
+
+(defn new-user!
+  ([username password]
+   (new-user! username password false))
+  ([username password admin?]
+   (uc/add-user!
+    (:users system)
+    {:username username
+     :password (bhash/derive password)
+     :is_admin admin?})))
