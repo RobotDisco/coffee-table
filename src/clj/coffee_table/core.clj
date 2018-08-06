48c58b3bf2cb354e908ea33562916632081118d4
Gaelan D'costa <gdcosta@gmail.com>
2018-08-05 20:06:41 -0400

    Re-add main func for prod invocation

--- src/clj/coffee_table/core.clj
+++ src/clj/coffee_table/core.clj
@@ -1,7 +1,7 @@
 (ns coffee-table.core
   "Entrypoint for production Uberjars"
   (:gen-class)
-  (:require
+  (:require [integrant.core :as ig]
             [coffee-table.system :refer [new-system]]
             [taoensso.timbre :as timbre]))
 
@@ -10,18 +10,18 @@
 (defn add-shutdown-hook! [^Runnable f]
   (.addShutdownHook (Runtime/getRuntime) (Thread. f)))
 
-#_ (defn logged-shutdown [system]
+(defn logged-shutdown [system]
   (info ::shutting-down-app)
-  (component/stop system)
+  (ig/halt system)
   (info ::shut-down-app))
 
-#_ (defn -main
+(defn -main
   [& args]
   (let [system (new-system :prod)]
     ;; Shut down system if we terminate
     (add-shutdown-hook! (partial logged-shutdown system))
     (info ::starting-app)
-    (component/start system)
+    (ig/init system)
     (info ::started-app))
   ;; All threads are daemon, so block forever:
   @(promise))
